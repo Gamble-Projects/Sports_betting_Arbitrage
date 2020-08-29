faf6ee56145137d9675a3ef6086e794263f3e731
Dor david Lasri <lasridor@gmail.com>
2020-08-29 18:55:07 +0300

    bseder

--- Arbitrage/Arbitrage/Program.cs
+++ Arbitrage/Arbitrage/Program.cs
@@ -6,7 +6,9 @@ namespace Arbitrage
     {
         public static void Main()
         {
-
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding("Windows-1255");
+            Console.WriteLine("אבאב שלכם זונה");
+            Console.ReadLine();
         }
     }
 }
