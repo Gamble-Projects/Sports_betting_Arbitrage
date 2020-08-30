c3e387f716ad06df4a7f3ad9b51bef7433303e81
Dor david Lasri <lasridor@gmail.com>
2020-08-30 19:13:42 +0300

    Scraper, ScraperWinner(in build) created

---
+++ Arbitrage/Arbitrage/Scraper.cs
@@ -0,0 +1,39 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Arbitrage
+{
+    abstract class Scraper
+    {
+        private readonly string r_WebsiteUrl;
+        private readonly HtmlWeb r_HtmlWeb;
+        private HtmlDocument m_HtmlDocument;
+
+        public string WebsiteUrl
+        {
+            get { return r_WebsiteUrl; }
+        }
+
+        public HtmlWeb HtmlWeb
+        {
+            get { return r_HtmlWeb; }
+        }
+
+        public HtmlDocument HtmlDocument
+        {
+            get { return m_HtmlDocument; }
+            set { m_HtmlDocument = value; }
+        }
+
+        public Scraper(string WebsiteUrl)
+        {
+            r_WebsiteUrl = WebsiteUrl;
+            r_HtmlWeb = new HtmlWeb();
+        }
+
+        abstract public void LoadUrl(); // throw Exception/event in case of connection not good
+        abstract public List<FootballMatch> MakeListOfDailyMatchesPlaying(); // if:m_HtmlDocument is not empty
+    }
+}
